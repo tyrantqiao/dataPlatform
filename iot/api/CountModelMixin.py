9443c5454cd600ab717749bdfe10b76e82f991a0
tyrantqiao <tyrantqiao@icloud.com>
2019-03-18 16:25:13 +0800

    update: test

--- iot/api/CountModelMixin.py
+++ iot/api/CountModelMixin.py
@@ -10,14 +10,3 @@ class CountModelMixin(object):
         queryset = self.filter_queryset(self.get_queryset())
         content = {'count': queryset.count()}
         return Response(content)
-
-    """
-    根据时间段返回list数据
-    """
-    @action(detail=False)
-    def get(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        start_date = self.request.query_params.get('start_date', None)
-        end_date = self.request.query_params.get('end_date', None)
-        if start_date or end_date:
-            queryset = queryset.filter(recordTime__range=[start_date, end_date])
--- iot/api/views.py
+++ iot/api/views.py
@@ -21,6 +21,17 @@ class DataListViewSet(viewsets.ModelViewSet, CountModelMixin):
     filter_backends = (filters.SearchFilter, DjangoFilterBackend,)
     search_fields = ('nodeId', 'recordTime', 'val', 'safe', 'unit')
     filter_fields = ('nodeId', 'recordTime', 'val', 'safe', 'unit')
+    
+    """
+    根据时间段返回list数据
+    """
+    @action(detail=False)
+    def get(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        start_date = self.request.query_params.get('start_date', None)
+        end_date = self.request.query_params.get('end_date', None)
+        if start_date or end_date:
+            queryset = queryset.filter(recordTime__range=[start_date, end_date])
 
 class SearchDataListViewSet(viewsets.ModelViewSet, CountModelMixin):
     """
